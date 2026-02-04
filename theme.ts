604992e4e87e8ea3508e81787b508538a9decbde
Manager Dayif <sekoudayifourouk@gmail.com>
2026-02-04 10:11:37 +0000

    add user store and theme configuration

---
+++ theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colorWhite: "#ffffff",
+  colorBlack: "#000000",
+  colorGreen: "#29b365",
+  colorLeafyGreen: "#206a42",
+  colorAppleGreen: "#a0d36c",
+  colorLimeGreen: "#d0e57e",
+  colorSecondary: "#f4f4f4",
+  fontSizeSmall: 12,
+  fontSizeMedium: 16,
+  fontSizeLarge: 24,
+};
