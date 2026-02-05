b915d66eb82504e68c0909b124da16deac22e238
Manager Dayif <sekoudayifourouk@gmail.com>
2026-02-05 19:33:17 +0000

    Implement FlatList for plant display and add PlantCard component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,30 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { PlantCard } from "@/components/PlantCard";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { usePlantStore } from "@/store/plantsSrore";
 import { theme } from "@/theme";
+import { useRouter } from "expo-router";
+import { FlatList, StyleSheet } from "react-native";
 
 export default function App() {
+  const router = useRouter();
+  const plants = usePlantStore((state) => state.plants);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //FlatList is a performant way to render a list of items in React Native, it only renders the items that are currently visible on the screen, and it also provides a lot of useful props for handling things like refreshing, pagination, etc.
+    //we should never use style props for styling the items in the list, instead we should use contentContainerStyle for styling the container of the list, and we should use the renderItem prop to render the items in the list, this way we can avoid unnecessary re-renders and improve performance
+    <FlatList
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      data={plants}
+      renderItem={({ item }) => <PlantCard plant={item} />}
+      ListEmptyComponent={
+        <PlantlyButton
+          title="Add your first plant"
+          onPress={() => {
+            router.navigate("/new");
+          }}
+        />
+      }
+    />
   );
 }
 
@@ -15,7 +32,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  contentContainer: {
+    padding: 12,
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,11 +1,13 @@
 export const theme = {
   colorWhite: "#ffffff",
   colorBlack: "#000000",
+  colorGrey: "#808080",
   colorGreen: "#29b365",
   colorLeafyGreen: "#206a42",
   colorAppleGreen: "#a0d36c",
   colorLimeGreen: "#d0e57e",
   colorSecondary: "#f4f4f4",
+  colorLightGrey: "#eee",
   fontSizeSmall: 12,
   fontSizeMedium: 16,
   fontSizeLarge: 24,
