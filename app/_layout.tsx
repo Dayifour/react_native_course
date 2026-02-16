1873f0aa9609c8cb03e44c7f34299fb6a33c4250
Manager Dayif <sekoudayifourouk@gmail.com>
2026-02-16 11:57:22 +0000

    Implement quick actions for adding plants in the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,25 @@
+import * as QuickActions from "expo-quick-actions";
+import { useQuickActionRouting } from "expo-quick-actions/router";
 import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { Platform } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  useQuickActionRouting();
+  useEffect(() => {
+    QuickActions.setItems([
+      {
+        title: "Add Plant",
+        subtitle: "Add a new plant to your collection",
+        icon: Platform.OS === "ios" ? "Symbole:leaf" : "leaf",
+        id: "0",
+        params: { href: "/new" },
+      },
+    ]);
+  }, []);
+
   //if we had more layouts, it would nest them in the app view view
   //so we can hide this tabs layout to avoid nesting tabs
   return (
